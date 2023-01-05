67079720d71924730422eefe5ef998d536569d9f
Ghosty <dan.polishuk@mail.ru>
2023-01-05 14:28:58 +0300

    update about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,5 @@
 
 @section('main_content')
 <h1>Страница про нас</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi perferendis enim, distinctio velit voluptas corporis magnam nobis sunt! Iure dolor alias dolore ab itaque nemo doloribus odio placeat libero expedita.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi perferendis enim, distinctio velit voluptas corporis magnam nobis sunt! Iure dolor alias dolore ab itaque nemo doloribus odio placeat libero expedita.123</p>
 @endsection
