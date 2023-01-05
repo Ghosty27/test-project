944e5cb3bbb51b99a6fc4dac71fc3504914d71f6
Ghosty <dan.polishuk@mail.ru>
2023-01-05 14:20:41 +0300

    Start

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('title')Про нас@endsection
+
+@section('main_content')
+<h1>Страница про нас</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi perferendis enim, distinctio velit voluptas corporis magnam nobis sunt! Iure dolor alias dolore ab itaque nemo doloribus odio placeat libero expedita.</p>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title')Главная страница@endsection
+
+@section('main_content')
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi perferendis enim, distinctio velit voluptas corporis magnam nobis sunt! Iure dolor alias dolore ab itaque nemo doloribus odio placeat libero expedita.</p>
+@endsection
