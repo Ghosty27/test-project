78a063840ec47e4f24ee537bbbe47bd32f298ac2
Ghosty <dan.polishuk@mail.ru>
2023-01-20 11:55:29 +0300

    MainController

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'MainController@home');
 
-Route::get('/gallery', function () {
-    return view('gallery');
-});
+Route::get('/gallery', 'MainController@gallery');
